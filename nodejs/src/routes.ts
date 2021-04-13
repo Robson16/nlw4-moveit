f899a7ac2dd1e1ab79383a2922f0ca84c25af3e3
Robson <robhenrod@gmail.com>
2021-04-12 21:53:20 -0300

    NodeJS - Day 3

--- nodejs/src/routes.ts
+++ nodejs/src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import UserController from './controllers/UserController';
+import SurveysController from './controllers/SurveysController';
 
 const router = Router();
 
 const userControler = new UserController();
+const surveyControler = new SurveysController();
 
 router.post('/users', userControler.create);
+
+router.post('/surveys', surveyControler.create);
+router.get('/surveys', surveyControler.show);
 
 export default router;
