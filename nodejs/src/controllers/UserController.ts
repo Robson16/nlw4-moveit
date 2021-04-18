b18091693a7b21a2566c6c41019d55d45cccbeef
Robson <robhenrod@gmail.com>
2021-04-18 13:40:10 -0300

    NodeJS - Day 5

--- nodejs/src/controllers/UserController.ts
+++ nodejs/src/controllers/UserController.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
+import AppError from '../errors/AppError';
 import UsersRepository from '../repositories/UsersRepository';
 
 class UserController {
   async create(request: Request, response: Response): Promise<any> {
     const { name, email } = request.body;
 
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error);
+    }
+
     const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({ email });
 
     if (userAlreadyExists) {
-      return response
-        .status(400)
-        .json({ error: 'This email is already in use.' });
+      throw new AppError('This email is already in use.');
     }
 
     const user = usersRepository.create({ name, email });
