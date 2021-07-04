7fa5ccc4adb952897875203bfc6178862f72969d
Robson <robhenrod@gmail.com>
2021-07-04 20:51:37 -0300

    ReactJS - Day 2

---
+++ reactjs/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/Robson16.png" alt="Robson" />
+      <div>
+        <strong>Robson H. Rodrigues</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
